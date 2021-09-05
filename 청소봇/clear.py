56d2b02e884eb8d6d91db3d4a4da0997eaeacaba
spider <89379428+SPIDER0122@users.noreply.github.com>
2021-09-05 13:22:03 +0900

    Update clear.py

--- 청소봇/clear.py
+++ 청소봇/clear.py
@@ -1,38 +1,28 @@
-
-###################
-####python 3.x#####
-#discord.py==1.4.0#
-###################
-
-import discord
-import asyncio
+# 파이썬의 기본 내장 함수가 아닌 다른 함수 혹은 다른 기능이 필요할 때 사용함
+import discord, asyncio
 
 client = discord.Client()
 
-
 @client.event
-async def on_ready():
-    print("봇 온라인 성공")
-    game = discord.Game('상태메시지 ~하는중')
-    await client.change_presence(status=discord.Status.online, activity=game)
-
+async def on_ready(): # 봇이 실행되면 한 번 실행됨
+    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("상태메시지 적으세요"))
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!청소'):
-        try:
-            # 메시지 관리 권한 있을시 사용가능
-            if message.author.guild_permissions.manage_messages:
-                amount = message.content[4:]
-                await message.delete()
-                await message.channel.purge(limit=int(amount))
-                message = await message.channel.send(embed=discord.Embed(title='🧹 메시지 ' + str(amount) + '개가 삭제되었습니다', colour=discord.Colour.green()))
-                await asyncio.sleep(2)
-                await message.delete()
-            else:
-                await message.channel.send('``명령어 사용권한이 없습니다.``')
-        except:
-            pass
+    if message.content.startswith ("!청소"):
+        if message.author.guild_permissions.administrator:
+            amount = message.content[4:]
+            await message.delete()
+            await message.channel.purge(limit=int(amount))
 
+            embed = discord.Embed(title="메시지 삭제 알림", description="최근 디스코드 채팅 {}개가\n관리자 {}님의 요청으로 인해 정상 삭제 조치 되었습니다".format(amount, message.author), color=0x000000)
+            embed.set_footer(text="Bot Made by = 𝕤𝕡𝕚𝕕𝕖𝕣#0001", icon_url="https://discordapp.com/channels/691615852620939274/703908401381376000/711859989177958410")
+            await message.channel.send(embed=embed)
+        
+        else:
+            await message.delete()
+            await message.channel.send("{}, 당신은 명령어를 사용할 수 있는 권한이 없습니다".format(message.author.mention))
 
-client.run('ODc2Mjc0Mjc1MzM2NzQwOTQ0.YRhsAw.LQh4USeEGgoFoLaTbFWP1pzdWX4')
+# 봇이 온라인이 되기 위한 토큰 입력
+client.run('bot token')
